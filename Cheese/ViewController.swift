09c816be464bdfe3a5d0d05e0cfee7711abaa2d6
Ted Lee <Ted@TedLee.me>
2016-06-20 21:24:22 -0700

    First commit

---
+++ Cheese/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cheese
+//
+//  Created by Ted Lee on 2016-06-20.
+//  Copyright © 2016 Moonshot Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
